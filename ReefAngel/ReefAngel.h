3cf86be231d633446b8011b58ebee083d881f356
Roberto Imai <info@reefangel.com>
2012-05-24 17:46:49 -0700

    Added AddRFExpansion()

--- ReefAngel/ReefAngel.h
+++ ReefAngel/ReefAngel.h
@@ -152,6 +152,7 @@ public:
 	void inline AddStandardMenu() {};
 	void inline AddWifi() {};
 	void inline AddDateTimeMenu() {};
+	void inline AddRFExpansion() {};
 	void StandardLights(byte LightsRelay, byte OnHour, byte OnMinute, byte OffHour, byte OffMinute);
 	void MHLights(byte LightsRelay, byte OnHour, byte OnMinute, byte OffHour, byte OffMinute, byte MHDelay);
 	void StandardHeater(byte HeaterRelay, int LowTemp, int HighTemp);
